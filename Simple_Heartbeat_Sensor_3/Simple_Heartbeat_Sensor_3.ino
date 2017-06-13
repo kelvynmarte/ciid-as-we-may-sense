f0981f9fa5106574ada8b31abe1b47a127d3f2a8
Kelvyn Ornette Sol Marte <kelvyn@kelvyn.net>
2017-06-13 19:14:53 +0200

    [TASK] Simple normalise of values

--- Simple_Heartbeat_Sensor_3/Simple_Heartbeat_Sensor_3.ino
+++ Simple_Heartbeat_Sensor_3/Simple_Heartbeat_Sensor_3.ino
@@ -1,4 +1,4 @@
-/*  PulseSensor™ Starter Project   http://www.pulsesensor.com
+                      q                 ^^^^  ^                     ^^  ^                                                           /*  PulseSensor™ Starter Project   http://www.pulsesensor.com
  *   
 This an Arduino project. It's Best Way to Get Started with your PulseSensor™ & Arduino. 
 -------------------------------------------------------------
@@ -14,10 +14,15 @@ This an Arduino project. It's Best Way to Get Started with your PulseSensor™ &
 //  Variables
 int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED13 = 13;   //  The on-board Arduion LED
-
-
-int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
+int pulse;                // holds the incoming raw data. Signal value can range from 0-1024
 int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore. 
+
+const int numReadings = 200;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
 
 
 // The SetUp Function:
@@ -30,22 +35,28 @@ void setup() {
 // The Main Loop Function
 void loop() {
 
-  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value. 
+  pulse = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value. 
                                               // Assign this value to the "Signal" variable.
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  readings[readIndex] = pulse;
+  total = total + readings[readIndex];
+  readIndex = readIndex + 1;
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+  average = total / numReadings;
+  // Serial.println("average: " + average);
 
-   Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
-
-   
-   if(Signal > Threshold){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.  
-     digitalWrite(LED13,HIGH);          
-   } else {
-     digitalWrite(LED13,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
-   }
+  Serial.println(pulse - average); 
 
 
-delay(10);
-   
-   
+ if(pulse > average){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.  
+   digitalWrite(LED13,HIGH);          
+ } else {
+   digitalWrite(LED13,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
+ }
+  delay(10);
 }
 
 
